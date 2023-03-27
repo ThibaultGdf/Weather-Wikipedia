a8d11815be8a49ad917bcf99756725825673a315
ThibaultGdf <thibaultgodef81@gmail.com>
2023-03-27 13:42:50 +0200

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Thibault GODEFROY on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
